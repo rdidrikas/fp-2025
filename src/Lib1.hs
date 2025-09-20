20a63856cfb83d262e96e6bda6ac039f67ec4d7e
Rokas <rdidrikas@gmail.com>
2025-09-20 15:16:00 +0000

    Added examples, fixed the bnf a little more

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -8,73 +8,13 @@ data Dumpable = Examples
 -- This is a "root" ADT representing your grammar,
 -- Please expand this ADT as needed
 
--- Top-level commands
-data Command
-    = meal MealBody
-    | Add Data MealType
-    | Remove Data MealType
-    | TotalCalories Date
-    | CurrentCalories
-    | Goal GoalOption
-    | Show Date
-    | DumpExamples
-    deriving (Show, Eq)
-
--- Meal can be recursive (one add, another meal, or nested meals)
-data MealBody
-    = SingleAdd Data MealType
-    | MultipleMeals [MealBody]
-    deriving (Show, Eq)
-
--- Data for food entries
-data Data = Data
-    { food     :: Food
-    , amount   :: Int
-    , unit     :: Unit
-    , calories :: Int
-    }
-    deriving (Show, Eq)
-
--- Food options
-data Food
-    = Apple | Banana | Chicken | Rice | Oats
-    | Milk | Egg | Bread | Salmon | Potato
-    | Beef | Pork | Yogurt | Cheese | Butter
-    | Tomato | Cucumber | Lettuce | Carrot | Onion
-    | Orange | Pear | Grapes | Strawberry | Blueberry
-    | Coffee | Tea | Water | Juice | Soda
-    | Pizza | Pasta | Burger | Sandwich | Soup
-    deriving (Show, Eq, Enum, Bounded)
-
-data Unit = Gram | Kilogram | Milliliter | Liter
-    deriving (Show, Eq)
-
--- Meal types
-data MealType = Breakfast | Lunch | Dinner | Snack
-    deriving (Show, Eq)
-
--- Dates
-data Date = Date
-    { year  :: Int
-    , month :: Int
-    , day   :: Int
-    }
-    deriving (Show, Eq)
-
--- Goal handling
-data GoalOption
-    = Set Date Int
-    | ShowGoal Date
-    | Compose [SingleGoal]
-    deriving (Show, Eq)
-
-data SingleGoal = SingleGoal Date Int
-    deriving (Show, Eq)
-
-data Command = Dump Dumpable
-  deriving Show
-
 examples :: [Command]
 examples = [
-    Dump Examples
+    "meal add lettuce, 7g, 5 kcal to lunch, add sandwich, 87l, 6 kcal to dinner, 
+    add cucumber, 8ml, 7 kcal to dinner, 
+    add bread, 8kg, 8 kcal to breakfast, 
+    add pork, 85kg, 5 kcal to snack",
+    "meal add tomato, 056l, 1 kcal to breakfast, add onion, 4l, 4 kcal to snack",
+    "goal compose 2025-08-28, 600, 2025-02-17, 3000, 2025-02-07, 1000",
+    "total 2014-06-23"
     ]
